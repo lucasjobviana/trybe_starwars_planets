2a83c2b99a8c105756e71d6d2a4bdfcafe55c2d7
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-06-02 14:35:53 -0300

    Refatorando for aninhados.

--- src/components/FilterByProperties.jsx
+++ src/components/FilterByProperties.jsx
@@ -5,20 +5,14 @@ import starWarsPlanetsContext from '../context/starWarsPlanetsContext';
 function FilterByProperties({ properties, id }) {
   const { addFilter, filter: {
     columnsFiltered } } = useContext(starWarsPlanetsContext);
-  let propertiesOptions = properties;
-  const newArray = [...properties];
-  if (columnsFiltered.length > 0) {
-    // console.log(properties, columnsFiltered);
-    properties.forEach((property, index) => {
-      columnsFiltered.forEach((column) => {
-      //  console.log(property, column);
-        if (property === column) {
-          delete newArray[index];
-        }
-      });
-    });
-    propertiesOptions = newArray;
-    // console.log(newArray);
+
+  if (columnsFiltered.length > 0 && id === 'column') {
+    properties = properties.reduce((propsFiltered, property) => {
+      if (!columnsFiltered.some((cF) => cF === property)) {
+        propsFiltered.push(property);
+      }
+      return propsFiltered;
+    }, []);
   }
 
   return (
@@ -33,7 +27,7 @@ function FilterByProperties({ properties, id }) {
         data-testid={ `${id}-filter` }
       >
         {
-          propertiesOptions.map((property, index) => (
+          properties.map((property, index) => (
             <option key={ `option${id}${index}` }>
               {property}
             </option>
